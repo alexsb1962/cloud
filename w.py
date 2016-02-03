aec3828a16b71cac761f59dd587b53dd21be384d
Alex S.Bagayev <alexsb@mail.ru>
2016-02-03 04:05:14 +0300

    First

---
+++ w.py
@@ -0,0 +1,13 @@
+import sys
+import struct
+def low_case_str( str ):
+    ...
+
+some_str='str for examplE СТРОКА для ПРИМЕРА'
+for symbol in some_str:
+    print(symbol)
+
+print(some_str.__sizeof__())
+
+
+print(some_str.lower())
