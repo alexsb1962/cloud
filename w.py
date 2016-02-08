b68b34204cd8bc26121efb073733c0e30e3cdd4b
Alex S.Bagayev <alexsb@mail.ru>
2016-02-09 00:03:39 +0300

    Добавляю модуль translit

--- w.py
+++ w.py
@@ -1,7 +1,26 @@
+#!usr/bih/env python3
 import sys
 import struct
-def low_case_str( str ):
-    ...
+import os
+import translit
+
+
+
+s= translit_str('This is Проверка')
+
+print(s)
+
+flist=os.listdir()
+print(flist)
+
+for fl in os.scandir('.'):
+    fname=fl.name
+    print('Очередное имя ',fname)
+    if fl.is_file():
+        os.rename(fl.name,low_case_str(fl.name))
+
+vd=[1,2,3,4,5,6,7,8,9]
+
 
 some_str='str for examplE СТРОКА для ПРИМЕРА'
 for symbol in some_str:
@@ -9,5 +28,5 @@ for symbol in some_str:
 
 print(some_str.__sizeof__())
 
-  
+
 print(some_str.lower())
