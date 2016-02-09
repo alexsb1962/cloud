1a8ff6228641f639d31177f6dacd24c213d57f08
Alex S.Bagayev <alexsb@mail.ru>
2016-02-09 04:55:10 +0300

    Как то работает

--- w.py
+++ w.py
@@ -2,31 +2,19 @@
 import sys
 import struct
 import os
-import translit
+from translit import translit_str
 
 
 
 s= translit_str('This is Проверка')
 
-print(s)
-
 flist=os.listdir()
 print(flist)
 
 for fl in os.scandir('.'):
-    fname=fl.name
-    print('Очередное имя ',fname)
     if fl.is_file():
-        os.rename(fl.name,low_case_str(fl.name))
+        try:
+            os.rename(fl.name,translit_str(fl.name))
+        except:
+            print('Не переименовано ',fl.name)
 
-vd=[1,2,3,4,5,6,7,8,9]
-
-
-some_str='str for examplE СТРОКА для ПРИМЕРА'
-for symbol in some_str:
-    print(symbol)
-
-print(some_str.__sizeof__())
-
-
-print(some_str.lower())
